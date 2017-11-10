24aa9c0561066feba3395e86afd0b2f2f368b010
marcuspanje <marcuspanje@gmail.com>
2017-11-09 17:14:42 -0800

    add util

--- generateFileNames.py
+++ generateFileNames.py
@@ -5,14 +5,6 @@ from random import shuffle
 from math import ceil
 import json
 
-#eg: given 'cs221_dataset/VGH_Training/0.1_280_5_5.jpg', 
-#returns: 'cs221_dataset/VGH_Training/labeled/1_280_5_5.jpg'
-def getLabeledName(fName):
-  findString = 'Training/'
-  i = fName.find(findString) + len(findString)
-  labeledName = fName[0:i] + 'labeled/' + fName[i+2:]
-  return labeledName
-
 #get dictionary of train,test,validate file names
 
 random.seed(10)
