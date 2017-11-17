1eceb55cbfd79bcc1fdef3b0fd042ff7c0deda0d
DMahajan <deeptism@umich.edu>
2017-11-17 03:40:42 +0000

    Add script to print max pixel value of jpgs

---
+++ testJpg.py
@@ -0,0 +1,22 @@
+from os.path import join, isfile
+import numpy as np
+from PIL import Image
+from os import listdir
+
+phases = ["train", "val", "test"]
+for phase in phases:
+	print (phase, ":")
+	data_dir = "/home/DeeptiM/cs221/cell_segmentation/cs221_dataset/"
+	label_path = join(data_dir + phase + '_labels.txt')
+	#label_list = [line.strip() for line in open(label_path, 'r')]
+	mypath = data_dir + "NKI_Training/labeled_preprocess/"
+	label_list = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	for label_file in label_list:
+		data = Image.open(join(mypath, label_file))
+		data_array = np.array(data, np.uint8)
+		if np.max(data_array) > 5:
+			print (label_file)
+			print (data_array.shape, np.min(data_array), np.max(data_array))
+		#assert np.min(data_array) >= 0
+		#assert np.max(data_array) < 3
+	
